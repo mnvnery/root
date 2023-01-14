88cc53a3ae2c0936b708e4378d7446efca62b618
Maria Nery <67118326+mnvnery@users.noreply.github.com>
2023-01-14 15:10:54 +0000

    Google

--- pages/_app.js
+++ pages/_app.js
@@ -9,10 +9,22 @@ function MyApp({ Component, pageProps }) {
       <meta name='viewport' content="width=device-width, initial-scale=1" />
       <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
       <link rel="shortcut icon" href="/favicon.ico" />
-      <title>Root</title>
+      <title>Root Media</title>
       <meta name='description' content='Root is a complete media planning and buying service for nonprofits—the only one in the UK.'/>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-NZBLC5W');
+        `}
+      </Script>
     </Head>
     <Component {...pageProps} />
+    <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NZBLC5W" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    </noscript>
     <Footer/>
     </>
   )
