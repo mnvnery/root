cbd9ad151783c61ab9846cd91b5e39df8650e1f7
Maria Nery <67118326+mnvnery@users.noreply.github.com>
2023-01-30 21:02:03 +0000

    gtm alternative

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import Head from 'next/head'
 import Footer from '../components/Footer'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -11,15 +12,15 @@ function MyApp({ Component, pageProps }) {
       <link rel="shortcut icon" href="/favicon.ico" />
       <title>Root Media</title>
       <meta name='description' content='Root is a complete media planning and buying service for nonprofits—the only one in the UK.'/>
-      <script id="google-tag-manager" strategy="afterInteractive">
-        {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-NZBLC5W');
-        `}
-      </script>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NZBLC5W');
+          `}
+      </Script>
     </Head>
     <Component {...pageProps} />
     <noscript>
