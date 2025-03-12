56850d381c3308f84e60d27bde27cec0d79b1e01
Maria Nery <mnvnery@gmail.com>
2025-03-12 11:28:40 +0000

    update sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.rootmedia.co.uk/',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
